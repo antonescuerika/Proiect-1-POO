a4593ce0556538740a670bec42748d54fd9a8974
antonescuerika <erikaantonescu07@gmail.com>
2026-03-25 19:14:26 +0200

    proiect 1 poo

--- proiect_1.cpp
+++ proiect_1.cpp
@@ -10,11 +10,13 @@ In urma rezervarii camera devine ocupata, iar costul total este calculat si afis
 */
 #include <iostream>
 #include <cstring>
+
+//Clasa pentru camerele de hotel
 class Camera{
     int numar;
     double pret;
     bool ocupat;
-    static int nrCamere;
+    static int nrCamere; // contor pentru numarul total de camere
 public:
     Camera();
     Camera(int numar, double pret, bool ocupat);
@@ -26,10 +28,13 @@ public:
     double getPret() const { return pret; }
     bool getOcupat() const { return ocupat; }
     static int getNrCamere(){ return nrCamere; }
-    inline void ocupa(){ ocupat = true; };
+
+    inline void ocupa(){ ocupat = true; }; //functie care marcheaza camera ca fiind ocupata
+
     friend std::ostream& operator<<(std::ostream& out, const Camera& c);
 };
 
+//Clasa pentru clienti
 class Client{
     char nume[50];
     char telefon[20];
@@ -43,10 +48,12 @@ public:
     friend std::ostream& operator<<(std::ostream& out, const Client& c);
 };
 
+//Clasa pentru rezervari
 class Rezervare{
     Client client;
     Camera camera;
     int zile;
+    //functie privata pentru calculul costului total
     double calculeazaCostIntern(double pret) const{
         return zile * pret;
     }
@@ -55,29 +62,35 @@ public:
     Rezervare(const Rezervare& r);
     Rezervare& operator=(const Rezervare& r);
     ~Rezervare();
+    //functie publica pentru calculul costului total
     double calculeazaCost(double pret) const{
         return calculeazaCostIntern(pret);
     };
     friend std::ostream& operator<<(std::ostream& out, const Rezervare& r);
 };
+//initializare variabila statica
 int Camera::nrCamere = 0;
+//constructor implicit camera
 Camera::Camera(){
     numar = 0;
     pret = 0;
     ocupat = false;
     nrCamere++;
 }
+//constructor cu parametri camera
 Camera::Camera(int numar, double pret, bool ocupat){
     this->numar = numar;
     this->pret = pret;
     this->ocupat = ocupat;
     nrCamere++;
 }
+//constructor de copiere camera
 Camera::Camera(const Camera& c){
     numar = c.numar;
     pret = c.pret;
     ocupat = c.ocupat;
 }
+//operator de atribuire camera
 Camera& Camera::operator=(const Camera& c){
     if(this != &c){
         numar = c.numar;
@@ -87,23 +100,28 @@ Camera& Camera::operator=(const Camera& c){
     return *this;
 }
 Camera::~Camera(){}
+
+//supraincarcarea operatorului<< (afisare camera)
 std::ostream& operator<<(std::ostream& out, const Camera& c){
     out << "Camera -> numar: " << c.numar << "; pret/noapte: " << c.pret << " lei; ocupata: " << (c.ocupat ? "da" : "nu");
     return out;
 }
-
+//constructor implicit client
 Client::Client(){
     strcpy(nume, "");
     strcpy(telefon, "");
 }
+//constructor cu parametri client
 Client::Client(const char* nume, const char* telefon){
         strcpy(this->nume, nume);
         strcpy(this->telefon, telefon);
 }
+//constructor de copiere client
 Client::Client(const Client& c){
     strcpy(nume, c.nume);
     strcpy(telefon, c.telefon);
 }
+//operator de atribuire client
 Client& Client::operator=(const Client& c){
     if(this != &c){
         strcpy(nume, c.nume);
@@ -112,17 +130,22 @@ Client& Client::operator=(const Client& c){
     return *this;
 }
 Client::~Client(){}
+
+//supraincarcarea operatorului<< (afisare client)
 std::ostream& operator<<(std::ostream& out, const Client& c){
     out << "Client -> nume: " << c.nume << "; telefon: " << c.telefon;
     return out;
 }
 
+//constructor cu parametri rezervare
 Rezervare::Rezervare(const Client& client, const Camera& camera, int zile){
     this->client = client;
     this->camera = camera;
     this->zile = zile;
 }
+//constructor de copiere rezervare
 Rezervare::Rezervare(const Rezervare& r): client(r.client), camera(r.camera), zile(r.zile){}
+//operator de atribuire rezervare
 Rezervare& Rezervare::operator=(const Rezervare& r){
     if(this != &r){
         client = r.client;
@@ -132,6 +155,8 @@ Rezervare& Rezervare::operator=(const Rezervare& r){
     return *this;
 }
 Rezervare::~Rezervare(){}
+
+//supraincarcarea operatorului<< (afisare rezervare)
 std::ostream& operator<<(std::ostream& out, const Rezervare& r){
     out << "Rezervare:\n" 
     << r.client << "\n" 
@@ -140,13 +165,16 @@ std::ostream& operator<<(std::ostream& out, const Rezervare& r){
     return out;
 }
 
+//functie pentru afisare camere
 void afiseazaCamere(Camera camere[], int n){
     for(int i = 0; i < n; i++){
         std::cout << i+1 << ". " << camere[i] << "\n";
     }
 }
+//functie pentru realizare rezervare
 void faRezervare(Camera camere[], int n, Client clienti[], int m){
     int camI, clientI;
+    //alegere camera
     std::cout << "Alege camera: ";
     std::cin >> camI;
     camI--;
@@ -154,14 +182,17 @@ void faRezervare(Camera camere[], int n, Client clienti[], int m){
         std::cout << "Index invalid!\n";
         return;
     }
+    //verificare daca camera e ocupata
     if(camere[camI].getOcupat()){
         std::cout << "Camera este deja ocupata!\n";
         return;
     }
+    //afisare clienti
     std::cout << "Alege client:\n";
     for(int i = 0; i < m; i++){
         std::cout << i+1 << ". " << clienti[i] << "\n";
     }
+    //alegere client
     std::cin >> clientI;
     clientI--;
     if(clientI < 0 || clientI >= m){
@@ -174,11 +205,13 @@ void faRezervare(Camera camere[], int n, Client clienti[], int m){
     std::cout << r;
 }
 int main(){
+    //initializare camere
     Camera camere[3] = {
         Camera(1, 100, false),
         Camera(2, 150, false),
         Camera(3, 200, true)
     };
+    //initializare clienti
     Client clienti[2] = {
         Client("Ionel", "0724592109"),
         Client("Maria", "0712345678")
@@ -207,4 +240,3 @@ int main(){
     }while(optiune != 0);
     return 0;
 }
-
